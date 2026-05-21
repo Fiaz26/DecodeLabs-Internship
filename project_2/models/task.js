16feb2f14efaa668b5242a70feff7f8451a558a7
Fiaz26 <fiaznns26@gmail.com>
2026-05-21 15:50:02 +0500

    Add task model with schema and validation

--- project_2/models/task.js
+++ project_2/models/task.js
@@ -1,1 +1,23 @@
+const mongoose = require("mongoose");
 
+const taskSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Please add task title"],
+    },
+    description: {
+      type: String,
+      required: [true, "Please add task description"],
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Task", taskSchema);
